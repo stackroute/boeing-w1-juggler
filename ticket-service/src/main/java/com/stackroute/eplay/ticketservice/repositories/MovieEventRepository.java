c925be46a1e89e3aea4b61651a9e7b9e54d4045a
abhishek <abhisheksingh4312@gmail.com>
2018-08-09 15:46:17 +0530

    Ticket service CRUD

---
+++ ticket-service/src/main/java/com/stackroute/eplay/ticketservice/repositories/MovieEventRepository.java
@@ -0,0 +1,18 @@
+package com.stackroute.eplay.ticketservice.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.stackroute.eplay.ticketservice.domain.MovieEvent;
+
+
+
+@Repository
+public interface MovieEventRepository extends MongoRepository<MovieEvent,String>{
+
+	
+//	
+//	@Query("From Movie m where m.title=:Title")
+//	public Iterable<Movie> getMovieByTitle(@Param("Title") String title);
+}
