b7b1ff3da1b04b9d5cf4d21ae6dff979d86bd304
rajputtinu7399 <rajputtinu7399@gmail.com>
2018-08-21 10:44:35 +0530

    search service providing complete data to movie info

--- Front-end/src/app/search-data.service.ts
+++ Front-end/src/app/search-data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import{Router} from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
+import { Movie } from './movie';
 
 @Injectable({
  providedIn: 'root'
@@ -12,10 +13,18 @@ export class SearchDataService {
  private messageSource = new BehaviorSubject('default message');
  currentMessage = this.messageSource.asObservable();
 
-
  changeMessage(message: string) {
-   this.messageSource.next(message)
- }
+  this.messageSource.next(message)
+}
+
+ private movie = new BehaviorSubject(new Movie());
+ movieMessage = this.movie.asObservable();
+
+ changeMovieMessage(message: Movie) {
+  this.movie.next(message)
+}
+
+ 
  constructor(private http: HttpClient, private router: Router) { }
  getMyEvents(city) {
    return this.http.get('http://172.23.238.221:8093/api/v1/city/'+city)
