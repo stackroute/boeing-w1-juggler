e63a7f5ef1063c311448b7a30e9bd07ddb3b4016
DeepakBalyan23 <deepakbalyan23@gmail.com>
2018-08-21 15:39:15 +0530

    changes

--- search-service/src/main/java/com/stackroute/eplay/search/domain/Show.java
+++ search-service/src/main/java/com/stackroute/eplay/search/domain/Show.java
@@ -3,6 +3,8 @@ package com.stackroute.eplay.search.domain;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +17,11 @@ public class Show {
 	private int showId;
 	private int seatRemaining;
 	private int price;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	private LocalTime startTime;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	private LocalDate date;
 	private int movieEventId;
 	private boolean status;
-	//private ArenaLayout layout;
+	// private ArenaLayout layout;
 }
