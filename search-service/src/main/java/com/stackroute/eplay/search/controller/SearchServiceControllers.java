48dd383bbf95c5132cc0f7fcbb5b71bd6520d7f3
DeepakBalyan23 <deepakbalyan23@gmail.com>
2018-08-21 09:29:49 +0530

    httpsessionf

--- search-service/src/main/java/com/stackroute/eplay/search/controller/SearchServiceControllers.java
+++ search-service/src/main/java/com/stackroute/eplay/search/controller/SearchServiceControllers.java
@@ -2,6 +2,7 @@ package com.stackroute.eplay.search.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
@@ -32,12 +33,15 @@ public class SearchServiceControllers {
 	public SearchServiceControllers(SearchServiceImpl searchService) {
 		this.searchService = searchService;
 	}
-
+	
+	private HttpServletRequest request;
+	private HttpSession session;
 	// get events by city name
 	@GetMapping("/city/{city}")
-	public ResponseEntity<?> getEventsBycity(@PathVariable String city, HttpSession session) {
+	public ResponseEntity<?> getEventsBycity(@PathVariable String city) {
 		try {
 			logger.info("Getting events in " + city);
+			session = request.getSession(); 
 			session.setAttribute("selectedCity", city);
 			System.out.println("====================="+session.getAttribute("selectedCity"));
 			return new ResponseEntity<Iterable<Movie>>(searchService.getEventsByCity(city), HttpStatus.OK);
@@ -89,6 +93,7 @@ public class SearchServiceControllers {
 	@GetMapping("/movie/{id}")
 	public ResponseEntity<?> getMovieById(@PathVariable int id, HttpSession session) {
 		System.out.println("Id "+id);
+		session = request.getSession(false); 
 		String city = (String) session.getAttribute("selectedCity");
 		System.out.println("---------------------------------"+ city);
 		System.out.println(session.getAttribute("selectedCity")+"---------------------------------");
