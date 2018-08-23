4373b2480d389f9443f9907109292df7aabbba33
DeepakBalyan23 <deepakbalyan23@gmail.com>
2018-08-23 20:18:10 +0530

    cross_origin

--- ticket-engine/src/main/java/com/stackroute/eplay/ticketengine/config/WebSocketConfig.java
+++ ticket-engine/src/main/java/com/stackroute/eplay/ticketengine/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/socket").withSockJS();
+		registry.addEndpoint("/socket").setAllowedOrigins("*").withSockJS();
 	}
 
 }
