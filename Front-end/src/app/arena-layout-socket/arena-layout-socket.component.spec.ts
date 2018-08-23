a25c604619bfe6c52df81e958953daddc00ab905
DeepakBalyan23 <deepakbalyan23@gmail.com>
2018-08-23 16:28:13 +0530

    socket_inti_try1

---
+++ Front-end/src/app/arena-layout-socket/arena-layout-socket.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArenaLayoutSocketComponent } from './arena-layout-socket.component';
+
+describe('ArenaLayoutSocketComponent', () => {
+  let component: ArenaLayoutSocketComponent;
+  let fixture: ComponentFixture<ArenaLayoutSocketComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ArenaLayoutSocketComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArenaLayoutSocketComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
