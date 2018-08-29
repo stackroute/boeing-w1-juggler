0e91628ccd307b0c12e2afb29a34e7fee4299554
manutyagi <manutyagi4@gmail.com>
2018-08-29 19:05:03 +0530

    movie-info page reload synopsis disappearing issue resolved

--- Front-end/src/app/movie-info/movie-info.component.ts
+++ Front-end/src/app/movie-info/movie-info.component.ts
@@ -10,12 +10,13 @@ export class MovieInfoComponent implements OnInit {
    movies: any;
   constructor(private _searchDataService:SearchDataService) {
     this.movies=JSON.parse(localStorage.getItem('movieInfo'));
+    console.log(this.movies);
 
   }
 
   ngOnInit() {
     this.movies=JSON.parse(localStorage.getItem('movieInfo'));
-    this._searchDataService.movieMessage.subscribe(movie => this.movie = movie);
+    this._searchDataService.movieMessage.subscribe(movies => this.movie = movies);
     console.log("my theatre",this.movie);
   }
 }
